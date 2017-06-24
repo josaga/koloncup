a8130b51b4633e8737799a50f1ab4d3bd0938ce2
josaga <josagaza@gmail.com>
2017-06-25 00:49:59 +0700

    1

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'sEx/LdMfuFWBoBbmk74lFFHGVhQWdwvujynTPsTloz//W1SQm+2d7wwZiX4yW5hYCCdKAAQfa6zcLB8b5qsCySrDVJS3GPlLHu/rnwV9a9uNHMoGRk1gfcTN4nvwPSDewwN40hczMJLCh3ASP6Zg4gdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
